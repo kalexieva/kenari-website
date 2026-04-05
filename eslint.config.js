29f93387bde29d2b5bb1bda8a1a14340b668160d
kalexieva <kalina.alexieva@keap.com>
2026-04-05 16:30:08 -0700

    Add Astro support for Prettier and ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,8 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+
 export default [
   {
-    ignores: ["dist/", "node_modules/", ".astro/"],
+    ignores: ["dist/**", "node_modules/**"],
   },
+  ...eslintPluginAstro.configs.recommended,
 ];
